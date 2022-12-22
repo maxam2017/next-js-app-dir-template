3c080165cfd21033b24f576cb08880d8ed0bd337
maxam2017 <whk0313@gmail.com>
2022-12-23 02:26:41 +0800

    feat: loading ui

--- app/dashboard/1/page.tsx
+++ app/dashboard/1/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+import { setTimeout } from "timers/promises";
+
+export default async function Page() {
+  await setTimeout(1000);
+
   return <div className="text-sm">This is dashboard 1.</div>;
 }
--- app/dashboard/2/page.tsx
+++ app/dashboard/2/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+import { setTimeout } from "timers/promises";
+
+export default async function Page() {
+  await setTimeout(1000);
+
   return <div className="text-sm">This is dashboard 2.</div>;
 }
